3cdf357e1aa2751775bc92d4a58b1045ec1e0225
Robert Hull <robertjhull@protonmail.com>
2021-12-26 23:12:59 -0800

    Change user data format

--- site.config.js
+++ site.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const projects = require('./src/data/projects');
+const { summary, projects, contactMethods } = require('./src/data/userData');
 
 module.exports = {
     config: {
@@ -8,20 +8,8 @@ module.exports = {
         previewDir: path.join(__dirname, './preview'),
         preview: false,
         layout: 'default',
-        title: `${process.env.NAME} | Portfolio`,
-        description: '',
-        summary: {
-            name: process.env.NAME,
-            occupation: process.env.OCCUPATION,
-            location: process.env.LOCATION,
-            summary: process.env.SUMMARY,
-        },
-        contactMethods: {
-            email: process.env.EMAIL,
-            github: process.env.GITHUB || null,
-            linkedin: process.env.LINKEDIN || null,
-        },
-        projects
-
+        summary,
+        projects,
+        contactMethods
     }
 }
